6ee31ba8c71ecb45c2af8ca34ac7056853193790
Elnura Dzhu <eln2806@gmail.com>
2023-06-23 10:25:11 -0500

    updating

--- src/flashcards.js
+++ src/flashcards.js
@@ -9,77 +9,77 @@ const flashcards = [
   {
     id: 2,
     category: "react",
-    question: "What is useState?",
+    question: "What is useState2?",
     answer:
       "use state is a react hook that allows us to render pages dynamically. ",
   },
   {
     id: 3,
     category: "react",
-    question: "What is useState?",
+    question: "What is useState3?",
     answer:
       "use state is a react hook that allows us to render pages dynamically. ",
   },
   {
     id: 4,
     category: "react",
-    question: "What is useState?",
+    question: "What is useState4?",
     answer:
       "use state is a react hook that allows us to render pages dynamically. ",
   },
   {
     id: 5,
     category: "react",
-    question: "What is useState?",
+    question: "What is useState5?",
     answer:
       "use state is a react hook that allows us to render pages dynamically. ",
   },
   {
     id: 5,
     category: "react",
-    question: "What is useState?",
+    question: "What is useState6?",
     answer:
       "use state is a react hook that allows us to render pages dynamically. ",
   },
   {
     id: 5,
     category: "react",
-    question: "What is useState?",
+    question: "What is useState7?",
     answer:
       "use state is a react hook that allows us to render pages dynamically. ",
   },
   {
     id: 6,
     category: "css",
-    question: "What is block element?",
+    question: "What is block element8?",
     answer:
       "block element is an html element which takes the whole width of its parent by default",
   },
   {
     id: 7,
     category: "css",
-    question: "What is block element?",
+    question: "What is block element?9",
     answer:
       "block element is an html element which takes the whole width of its parent by default",
   },
   {
     id: 8,
     category: "css",
-    question: "What is block element?",
+    question: "What is block element10?",
     answer:
       "block element is an html element which takes the whole width of its parent by default",
   },
   {
     id: 9,
     category: "css",
-    question: "What is block element?",
+    question: "What is block element11?",
     answer:
       "block element is an html element which takes the whole width of its parent by default",
   },
   {
     id: 10,
     category: "css",
-    question: "What is block element?",
+    question: "What is block element12?",
     answer:
       "block element is an html element which takes the whole width of its parent by default",
   },
